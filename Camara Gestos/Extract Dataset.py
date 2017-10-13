badd7ef79c880984609151024e4234b6828d4cb8
piero0407 <piero.barbieri50@gmail.com>
2017-10-13 16:39:53 -0500

    xD

--- Camara Gestos/Extract Dataset.py
+++ Camara Gestos/Extract Dataset.py
@@ -12,10 +12,10 @@ f = gz.open('dataset.pkl.gz', 'rb')
 training_data, validation_data = pkl.load(f, encoding='latin1')
 
 x_train = training_data[0]
-# y_train = (np.arange(numOfLabels) == training_data[1][:, None]).astype(np.float32)
+y_train = (np.arange(numOfLabels) == training_data[1][:, None]).astype(np.float32)
 
 x_validation = validation_data[0]
-# y_validation = (np.arange(numOfLabels) == validation_data[1][:, None]).astype(np.float32)
+y_validation = (np.arange(numOfLabels) == validation_data[1][:, None]).astype(np.float32)
 
 plt.imshow(x_train[randomNum].reshape(100, 100), cmap='gray')
 plt.show()
